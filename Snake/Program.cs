da9bba74a7266c401ea3be52c077b107059dfaaf
StudentUWM <kacper19@vp.pl>
2016-12-07 19:15:50 +0100

    Update Program.cs

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -8,9 +8,7 @@ namespace Snake
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+      
         [STAThread]
         static void Main()
         {
